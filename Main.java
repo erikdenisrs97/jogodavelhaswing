321ac3e080ad65e5831f364c40dd81c782071c8b
Erik Denis <erikdenisrs97@gmail.com>
2021-04-25 22:59:19 -0300

    update

--- Main.java
+++ Main.java
@@ -9,11 +9,16 @@ public class Main {
   private static int size1 = 3;
   private static final int PLAYER_1 = 1;
   private static final int PLAYER_2 = 2;
-  int playerTime = 1;
+  private String player1Name = "";
+  private String player2Name = "";
+  private int playerTime = 1;
+  private int round = 0;
+  private String winner = "";
 
   // Layout
   JFrame frame = new JFrame();
   JPanel southPanel = new JPanel();
+  JPanel northPanel = new JPanel();
   JButton button[][] = new JButton[size1][size1];
   JMenuBar mb = new JMenuBar();
   JMenu newGame = new JMenu();
@@ -21,6 +26,7 @@ public class Main {
   JMenu exit = new JMenu();
   JMenuItem computer = new JMenuItem();
   JMenuItem player = new JMenuItem();
+  JLabel whoPlays = new JLabel();
 
   public Main() {
     initGUI();
@@ -46,7 +52,7 @@ public class Main {
    for(int i = 0; i < size1; i++) {
      for(int j = 0; j < size1; j++) {
        button[i][j] = new JButton();
-       southPanel.add(button[i][j]);
+       southPanel.add(button[i][j]).setLocation(i, j);
        button[i][j].setEnabled(false);
      }
    }
@@ -62,6 +68,13 @@ public class Main {
    computer.setText("Contra o computador");
    newGame.add(player);
    player.setText("Contra um jogador");
+
+   // North Panel Settings
+   northPanel.setPreferredSize(new Dimension(290, 150));
+   frame.getContentPane().add(northPanel, BorderLayout.NORTH);
+   
+   // JLabel Settings
+   northPanel.add(whoPlays);
 
    frame.setJMenuBar(mb);
    frame.setVisible(true);
@@ -88,7 +101,10 @@ public class Main {
     });
   }
 
-  private void logic() {
+  private void playerLogic() {
+    clean();
+    questionNames();
+    whoPlays.setText("É a vez do(a) " + player1Name + " jogar.");
     for(int k = 0; k < size1; k++) {
       for(int l = 0; l < size1; l++) {
         JButton currentButton = button[k][l];
@@ -98,8 +114,37 @@ public class Main {
           if(playerTime == PLAYER_1) {
             currentButton.setText("X");
             playerTime = PLAYER_2;
+            whoPlays.setText("É a vez do(a) " + player2Name + " jogar.");
           } else {
             currentButton.setText("O");
+            whoPlays.setText("É a vez do(a) " + player1Name + " jogar.");
+            playerTime = PLAYER_1;
+          }
+          boolean endGame = verifyWin();
+          if(endGame) {
+            continueGame();
+          }
+        });
+      }
+    }
+  }
+
+  private void computerLogic() {
+    clean();
+    player1Name = JOptionPane.showInputDialog(null, "Digite o seu nome: ");
+    player2Name = "COMPUTADOR";
+    whoPlays.setText("É a vez do(a) " + player1Name + " jogar.");
+    for(int k = 0; k < size1; k++) {
+      for(int l = 0; l < size1; l++) {
+        JButton currentButton = button[k][l];
+        currentButton.setEnabled(true);
+        currentButton.addActionListener(e -> {
+          if(!currentButton.getText().equals("")) return ;
+          if(playerTime == PLAYER_1) {
+            currentButton.setText("X");
+            playerTime = PLAYER_2;
+            whoPlays.setText("É a vez do(a) " + player2Name + " jogar.");
+            randomPlay();
             playerTime = PLAYER_1;
           }
           boolean endGame = verifyWin();
@@ -123,21 +168,39 @@ public class Main {
 
   private void playPlayer() {
     player.addActionListener(e -> {
-      logic();
+      playerLogic();
     });
   }
 
   private void playComputer() {
     computer.addActionListener(e -> {
-
+      computerLogic();
     });
+  }
+
+  private void questionNames() {
+    player1Name = JOptionPane.showInputDialog(null, "O nome do 1º Jogador: ");
+    player2Name = JOptionPane.showInputDialog(null, "O nome do 2º Jogador: ");
   }
 
   private void clean() {
     for(int m = 0; m < size1; m++){
       for(int n = 0; n < size1; n++) {
         button[m][n].setText("");
+        round = 0;
       }
+    }
+  }
+
+  private void randomPlay() {
+    double randDouble1 = Math.random() * 2;
+    double randDouble2 = Math.random() * 2;
+    int int1 = (int)Math.round(randDouble1);
+    int int2 = (int)Math.round(randDouble2);
+    if(button[int1][int2].getText().equals("")) {
+      button[int1][int2].setText("0");
+    } else {
+      randomPlay();
     }
   }
 
@@ -154,30 +217,93 @@ public class Main {
 
     // Line
     if(!b00.equals("") && b00.equals(b01) && b00.equals(b02)){
+      if(b00.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
+
       return true;
     }
     if(!b10.equals("") && b10.equals(b11) && b10.equals(b12)){
+      if(b10.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
       return true;
     }
     if(!b20.equals("") && b20.equals(b21) && b20.equals(b22)){
+      if(b20.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
       return true;
     }
     // Colum
     if(!b00.equals("") && b00.equals(b10) && b00.equals(b20)){
+      if(b00.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
       return true;
     }
     if(!b01.equals("")  && b01.equals(b11) && b01.equals(b21)){
+      if(b01.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
       return true;
     }
     if(!b02.equals("") && b02.equals(b12) && b02.equals(b22)){
+      if(b02.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
       return true;
     }
     // Diagonal
     if(!b00.equals("") && b00.equals(b11) && b00.equals(b22)){
+      if(b00.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
       return true;
     }
     if(!b02.equals("")  && b02.equals(b11) && b02.equals(b20)){
+      if(b02.equals("X")) {
+        winner = player1Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player1Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      } else {
+        winner = player2Name;
+        JOptionPane.showMessageDialog(null, "O ganhador foi: " + player2Name + ".", "Fim de Jogo", JOptionPane.INFORMATION_MESSAGE);
+      }
       return true;
+    }
+
+    round++;
+    if(round == 9){
+      JOptionPane.showMessageDialog(null, "Deu véia!", "Véia", JOptionPane.INFORMATION_MESSAGE);
+      continueGame();
     }
 
     return false; 
